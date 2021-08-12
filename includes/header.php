062b787f332831e27887095d7df80b706c0c2839
Nicolas Demange <nicolas.demange@engie.com>
2021-08-12 10:26:29 +0200

    Replace flex player by react one.

--- includes/header.php
+++ includes/header.php
@@ -2,64 +2,31 @@
 <html lang="en" class="no-js">
 <head>
   <meta charset="utf-8">
-  <!-- www.phpied.com/conditional-comments-block-downloads/ -->
-  <!--[if IE]><![endif]-->
-
-  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
-       Remove this if you use the .htaccess -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
   <title>BonsMots.fr - Mots Fl&eacute;ch&eacute;s et Mots Crois&eacute;s en ligne</title>
   <link rel="alternate" type="application/rss+xml" title="RSS 2.0 - Les Derni�res Grilles Publi�s" href="https://feeds.feedburner.com/Bonsmotsfr" />
 
   <meta name="description" content="">
   <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <!--  Mobile Viewport Fix
-        j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
-  device-width : Occupy full width of the screen in its current orientation
-  initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
-  maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
-  -->
-  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
-
-
-  <!-- Place favicon.ico and apple-touch-icon.png in the root of your domain and delete these references -->
   <link rel="shortcut icon" href="https://bonsmots.fr/favicon.ico">
   <link rel="apple-touch-icon" href="https://bonsmots.fr/apple-touch-icon.png">
 
-
-  <!-- CSS : implied media="all" -->
   <link rel="stylesheet" href="https://bonsmots.fr/css/style.css?v=1">
-
-  <!-- For the less-enabled mobile browsers like Opera Mini -->
   <link rel="stylesheet" media="handheld" href="https://bonsmots.fr/css/handheld.css?v=1">
-
-  <!-- The website stylesheet -->
   <link rel="stylesheet" href="https://bonsmots.fr/css/arrows.css">
- 
-  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
+
   <script src="https://bonsmots.fr/js/modernizr-1.5.min.js"></script>
 
-  <!-- BonsMots.fr: if this is the flex grid player page, we add the required FLEX header lines here -->
-<?php
-if ($is_grid){
-  include("flex-player/player-header.php");
-}else if($is_editor){
-  include("flex-editor/editor-header.php");
-}
-?>
 
+  <?php if($is_grid){ ?>
+        <link rel="manifest" href="/includes/react-player/manifest.json"/>
+        <link rel="stylesheet" href="/includes/react-player/styles.css"/>
+        <link href="/includes/react-player/static/css/main.bc3b2b68.chunk.css" rel="stylesheet">
+  <?php } ?>
 </head>
 
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-
-<!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
-<!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
-<!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
-<!--[if IE 9 ]>    <body class="ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <body> <!--<![endif]-->
-
+<body>
 <div id="global">
 <header id="page-header">
 
